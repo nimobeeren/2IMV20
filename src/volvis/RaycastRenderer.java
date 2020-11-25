438a66e89a4094860d15bcfb798f9128b000c9a1
Nimo Beeren <nimobeeren@gmail.com>
2020-11-25 17:42:36 +0100

    remove unused import

--- src/volvis/RaycastRenderer.java
+++ src/volvis/RaycastRenderer.java
@@ -8,7 +8,6 @@ import gui.RaycastRendererPanel;
 import gui.TransferFunction2DEditor;
 import gui.TransferFunctionEditor;
 import java.awt.image.BufferedImage;
-import java.util.Date;
 import util.TFChangeListener;
 import util.VectorMath;
 import volume.GradientVolume;
