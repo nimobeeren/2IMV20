e4dc5c924c3f22cd8b38787adfb8e51534f39e75
Nimo Beeren <nimobeeren@gmail.com>
2021-01-09 22:17:27 +0100

    fit map to container

--- InfoVis/client/src/App.svelte
+++ InfoVis/client/src/App.svelte
@@ -1,34 +1,14 @@
 <script>
-  import Test from "./Test.svelte";
   import Map from "./Map.svelte";
-
-  export let name;
 </script>
 
 <style>
   main {
-    text-align: center;
     padding: 1em;
-    max-width: 240px;
     margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
   }
 </style>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <Test />
   <Map />
 </main>
--- InfoVis/client/src/Map.svelte
+++ InfoVis/client/src/Map.svelte
@@ -2,33 +2,51 @@
   import { geoPath, geoMercator } from "d3-geo";
   import { onMount } from "svelte";
 
-  let data;
+  let width;
+  let height;
+  let earth;
+  let pathData;
 
+  const projection = geoMercator();
+  const path = geoPath().projection(projection);
+
+  // Get Earth geometry data
   onMount(async function () {
-    const projection = geoMercator();
-    projection.center([15, 15]);
-    const path = geoPath().projection(projection);
-
     const response = await fetch(
       "https://raw.githubusercontent.com/martynafford/natural-earth-geojson/master/50m/cultural/ne_50m_admin_0_countries.json"
     );
-    const json = await response.json();
-
-    data = path(json);
+    earth = await response.json();
+    pathData = path(earth);
   });
+
+  // Resize the map if needed
+  $: {
+    projection.fitSize([width, height], earth);
+    pathData = path(earth);
+  }
 </script>
 
 <style>
-  svg {
-    width: 960px;
+  div {
+    max-width: 960px;
     height: 500px;
+    margin: 0 auto;
+    border: 1px solid orange;
   }
+
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+
   .border {
     stroke: #444;
     fill: #ccc;
   }
 </style>
 
-<svg width="960" height="500">
-  <path d={data} class="border" />
-</svg>
+<div bind:clientWidth={width} bind:clientHeight={height}>
+  <svg>
+    <path class="border" d={pathData} />
+  </svg>
+</div>
