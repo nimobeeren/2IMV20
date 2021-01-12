60251354af9ae02d333f6f9ccc79cfef69b9f562
Nimo Beeren <nimobeeren@gmail.com>
2021-01-12 13:40:48 +0100

    set scale correctly

--- InfoVis/client/src/Map.svelte
+++ InfoVis/client/src/Map.svelte
@@ -2,27 +2,48 @@
   import { geoPath, geoMercator } from "d3-geo";
   import { onMount } from "svelte";
 
-  let width;
-  let height;
+  let viewW;
+  let viewH;
   let earth;
   let pathData;
+
+  const targetDeg = [
+    [-30, 80], // top-left
+    [60, -40], // bottom-right
+  ];
 
   const projection = geoMercator();
   const path = geoPath().projection(projection);
 
   // Get Earth geometry data
   onMount(async function () {
-    const response = await fetch(
-      "https://raw.githubusercontent.com/martynafford/natural-earth-geojson/master/50m/cultural/ne_50m_admin_0_countries.json"
-    );
+    const response = await fetch("/data/earth.json");
     earth = await response.json();
     pathData = path(earth);
   });
 
   // Resize the map if needed
   $: {
-    projection.fitSize([width, height], earth);
-    pathData = path(earth);
+    if (viewW !== undefined && viewH !== undefined) {
+      const target = [
+        projection(targetDeg[0]), // top-left
+        projection(targetDeg[1]), // bottom-right
+      ];
+      const targetW = target[1][0] - target[0][0];
+      const targetH = target[1][1] - target[0][1];
+
+      // const centerPx = [target[0][0] + targetW / 2, target[0][1] + targetH / 2];
+      // console.log(centerPx);
+      // const center = projection([0, 1]);
+      // console.log(center);
+      // projection.translate([400, 200]);
+
+      const currScale = projection.scale();
+      const newScale = currScale * Math.min(viewW / targetW, viewH / targetH);
+      projection.scale(newScale);
+
+      pathData = path(earth);
+    }
   }
 </script>
 
@@ -45,7 +66,7 @@
   }
 </style>
 
-<div bind:clientWidth={width} bind:clientHeight={height}>
+<div bind:clientWidth={viewW} bind:clientHeight={viewH}>
   <svg>
     <path class="border" d={pathData} />
   </svg>
