dc814099fbc980406040382fe1e203b69701f735
Nimo Beeren <nimobeeren@gmail.com>
2021-01-24 16:48:36 +0100

    prevent zooming out too far

--- InfoVis/app/src/LeafletMap.svelte
+++ InfoVis/app/src/LeafletMap.svelte
@@ -23,6 +23,7 @@
   const defaultOptions = {
     zoomControl: false,
     maxBounds: BOUNDS,
+    minZoom: 1.6,
     zoomSnap: 0.1,
     crs: L.CRS.EPSG4326,
     maxBoundsViscosity: 1.0, // prevent panning outside bounds
